b2d5505ac831708abde3bce0742be165c1d9cc29
tehminaansari <tehmina.ansari@infobeans.com>
2016-12-21 13:53:46 +0530

    Magento2- Social login module

---
+++ app/code/Infobeans/SocialLogin/Block/Popup/Create.php
@@ -0,0 +1,14 @@
+<?php
+namespace Infobeans\SocialLogin\Block\Popup;
+
+ use Magento\Customer\Block\Form\Register;
+
+class Create extends Register
+{
+	protected function _prepareLayout()
+    {
+       
+    }
+
+}
+
---
+++ app/code/Infobeans/SocialLogin/Block/System/Config/Form/Field/Redirect/Twitter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Infobeans\SocialLogin\Block\System\Config\Form\Field\Redirect;
+
+use Infobeans\SocialLogin\Block\System\Config\Form\Field\Redirect;
+
+/**
+ * Backend system config datetime field renderer
+ */
+class Twitter extends Redirect
+{
+	protected $socialType = 'Twitter';
+}
